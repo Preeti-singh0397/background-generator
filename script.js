5520d644b7c3aba9a0021233c1f3c5ff20981196
Preeti-singh0397 <63150571+Preeti-singh0397@users.noreply.github.com>
2020-04-17 19:45:18 +0530

    adding starting project

---
+++ script.js
@@ -0,0 +1,35 @@
+var css=document.querySelector('h3');
+var color1=document.getElementById('color1');
+var color2=document.getElementById('color2');
+var body=document.getElementById('graident');
+
+// console.log(css);
+// console.log(color1);
+// console.log(color2);
+// function set() {
+//     body.style.background=
+//   "linear-gradient(to-right," 
+//   + color1.value 
+//   + ","
+//   + color2.value
+//   +")";
+// }
+function setGradient() {
+    body.style.background = 
+    "linear-gradient(to right, " 
+    + color1.value 
+    + ", " 
+    + color2.value 
+    + ")";
+
+    
+ 
+
+    css.textContent = body.style.background + ";";
+}
+
+
+
+// color1.addEventListener("input",setGradient);
+
+// color2.addEventListener("input",setGradient);
